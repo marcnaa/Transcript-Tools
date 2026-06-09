a771a1e37ab06b5b0769329205e6fbbe23615f0a
Marc Noguera <marc.noguera@nemeda.io>
2026-06-09 15:42:58 +0200

    Initial Commit

---
+++ Transcript Tools/Transcript_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Transcript_ToolsApp.swift
+//  Transcript Tools
+//
+//  Created by Marc Noguera on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Transcript_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
